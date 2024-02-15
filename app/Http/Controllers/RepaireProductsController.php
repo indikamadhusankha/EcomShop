ffaa159ff46cf91e4270baddbef4d75e3a7c8a2c
Indika Madhusankha <indikamadushanka91@gmail.com>
2024-02-15 12:33:59 +0530

    check repaire product store

--- app/Http/Controllers/RepaireProductsController.php
+++ app/Http/Controllers/RepaireProductsController.php
@@ -2,27 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use App\Models\RepaireProduct;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class RepaireProductsController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('pages.products.repaire-products');
     }
 
 
-    public function store(Request $request){
-        $product = DB::table('products')->where('sku',$request->sku)->first();
-        if(empty($product->sku)){
+    public function store(Request $request)
+    {
 
+        $product = DB::table('products')->where('sku', $request->sku)->first();
+        if (empty($product->sku)) {
 
-        }else{
-            $repaire_product = new RepaireProduct;
-            $repaire_product->sku = $request->sku;
-            $repaire_product->save();
+            return redirect()->route('Products.repaire')->with('Errors', 'Product Not Found!');
+        } else {
+            $product = DB::table('repaire_products')->where('sku', $request->sku)->first();
+            if ($product->id > 0) {
+                return redirect()->route('Products.repaire')->with('Errors', 'Product Already Added!');
+            } else {
+
+                $repaire_product = new RepaireProduct;
+                $repaire_product->sku = $request->sku;
+                $repaire_product->save();
+
+                Product::where('sku', $product->sku)->update(['sale_status' => 3]);
+                return redirect()->back();
+            }
         }
-
     }
 }
--- resources/views/components/messages.blade.php
+++ resources/views/components/messages.blade.php
@@ -11,7 +11,7 @@
 
 @if (Session::has('Errors'))
 <div id="alert" class="alert alert-danger alert-dismissible fade show" role="alert">
-    <h4 class="alert-heading">Success!</h4>
+    <h4 class="alert-heading">Error!</h4>
     <p>{{ Session::get('Errors') }}</p>
 
     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
